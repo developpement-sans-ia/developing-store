9589269c9370fe8731eadf334dbf2f9c8d9499e2
Quentin <passeboscq@gmail.com>
2026-07-23 16:26:05 +0200

    filtres fonctionnent

--- dev-sans-ia/src/app/shared/card-product/card-product.ts
+++ dev-sans-ia/src/app/shared/card-product/card-product.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
+import { CardProductDetail } from '../card-product-detail/card-product-detail';
 import { Product } from '../types';
 
 
 @Component({
   selector: 'app-card-product',
-  imports: [],
+  imports: [CardProductDetail],
   templateUrl: './card-product.html',
   styleUrl: './card-product.css',
 })

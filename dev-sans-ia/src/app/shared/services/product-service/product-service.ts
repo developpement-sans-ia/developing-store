54842445e93a59b1da099b3dc32287065beb4d28
Quentin <passeboscq@gmail.com>
2026-07-23 11:25:29 +0200

    doublon supprimé

--- dev-sans-ia/src/app/shared/services/product-service/product-service.ts
+++ dev-sans-ia/src/app/shared/services/product-service/product-service.ts
@@ -1,4 +1,24 @@
-import { Service } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { inject, Service, signal } from "@angular/core";
+import { Observable, tap } from "rxjs";
+import { Product } from "../../types";
 
 @Service()
-export class ProductService {}
+export class ProductService {
+    private readonly http = inject(HttpClient);
+    private productList = signal<Product[]>([]);
+    readonly productListSignal = this.productList.asReadonly();
+    private product = signal<Product>({} as Product);
+    private productSignal = this.product.asReadonly();
+    readonly url = "https://fakestoreapi.com/products";
+
+    getProductList(): Observable<Product[]>{
+        return this.http.get<Product[]>(this.url)
+        .pipe(tap((response : Product[])=> this.productList.set(response)));
+    }
+
+    getOneProduct(id:string):Observable<Product>{
+        return this.http.get<Product>(this.url+`/${id}`)
+        .pipe(tap((response: Product)=>this.product.set(response)));
+    }
+}

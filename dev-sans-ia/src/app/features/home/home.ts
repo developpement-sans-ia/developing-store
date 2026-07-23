35e2fff71ef57011f1c5307a78a61a0f1a4f25db
Fanny Saez <saez.fanny.63@gmail.com>
2026-07-23 20:39:07 +0200

    feat(home): affichage des 3 dernières nouveautés + fix @Injectable bloquant

--- dev-sans-ia/src/app/features/home/home.ts
+++ dev-sans-ia/src/app/features/home/home.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CardProduct } from '../../shared/card-product/card-product';
+import { ProductService } from '../../shared/services/product-service/product-service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, CardProduct],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit {
+  private readonly productService = inject(ProductService);
+  private readonly productList = this.productService.productListSignal;
+  readonly latestProducts = computed(() => this.productList().slice(0, 3));
+
+  ngOnInit() {
+    this.productService.getProductList().subscribe();
+  }
+}

66cdaf2443ce5686af31e2adabe9a09e8531cd45
Sabrina <li.chidori.kinomoto@gmail.com>
2026-07-24 09:42:02 +0200

    correction problème panier

--- dev-sans-ia/src/app/features/cart/cart.ts
+++ dev-sans-ia/src/app/features/cart/cart.ts
@@ -1,14 +1,18 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { CardProductDetail } from '../../shared/card-product-detail/card-product-detail'
+import { Product } from '../../shared/types';
+import { ProductService } from '../../shared/services/product-service/product-service';
 
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CardProductDetail],
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
 export class Cart {
+  productService = inject(ProductService);
+  product = this.productService.productSignal;
   cart = [2, 2, 2];
 
 }

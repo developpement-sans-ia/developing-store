606fb32c294600d286b7d4d01268892c5f63b723
Quentin <passeboscq@gmail.com>
2026-07-24 09:26:05 +0200

    user login added

--- dev-sans-ia/src/app/app.routes.ts
+++ dev-sans-ia/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Home } from './features/home/home';
 import { Products } from './features/product/products/products';
 import { Cart } from './features/cart/cart';
 import { Page404 } from './features/page404/page404';
+import { UserPage } from './features/userPage/user-page/user-page';
+import { UserPageDetail } from './features/userPage/user-page-detail/user-page-detail/user-page-detail';
 
 export const routes: Routes = [
   { path : '', component : Home},
   { path : 'product', component : Products},
   { path : 'cart', component : Cart},
   { path : '404', component : Page404},
+  { path : 'user', component : UserPage},
+  { path : 'userlogedin', component : UserPageDetail}
 ];
--- dev-sans-ia/src/app/shared/types.ts
+++ dev-sans-ia/src/app/shared/types.ts
@@ -12,10 +12,30 @@ export interface User{
     username: string;
     email: string;
     password: string;
+    address: AddressUser;
+    phone: string;
+    name: Name;
 }
 
 export interface CartInt{
     id:number;
     userId:number;
     products:Product[]
+}
+
+interface AddressUser {
+    city: string;
+    street: string;
+    number: number;
+    zipcode: string;
+}
+
+interface Name {
+    firstname: string;
+    lastname: string;
+}
+
+export interface loginData{
+    email:string;
+    password:string;
 }
